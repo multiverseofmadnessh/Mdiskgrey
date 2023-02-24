a04dfb12fd9a33b2af5f0a667ebade738b410a2e
multiverseofmadnessh <124427766+multiverseofmadnessh@users.noreply.github.com>
2023-02-24 12:35:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,11 +4,11 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "13290427"))
     API_HASH = os.environ.get("API_HASH", "c33b2f280810fc2f60a6387a4c4217f2")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5015404755:AAE58laq-OGoqxkKth7TjVzyKYBTa4zs7oc")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5894919927:AAHTPcseEL_HRJE7ik2SVGKa0nhKCqXSkE0")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "1BVtsOLsBu7SJS71LBT0H9mAGqMi1hvPRLSO_zDQL3GwMwoGhx_0F21cOxz_AAMIVDMSTBSh0oz7FISaWAdrnwKyZX3iOsupa4Fp-v8BTkPy71k6J-vMEFqJXn2UKNqbRFs2I2HhVCkrHHsRefxBHOdOKU5P18RHaZ4OCdMWzAQrr_MLsz9cn1LK7dfRGhO3SV4TpIL7R9UtzTLZP4wCz_y6IgLwWu3pDoEI_FII8534WJUUVrexaOpBeCSxeTlZ7SdMcPY4INhNqcsFAaji82kah6Hb8dNw15SQuDVPxFiU4dLdhy2mBDb3MkGENsfBOnUaHq3dpM9vpfmXtBe9NWskPzOHrrFA=")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001711211283"))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Arymovies_bot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Mdisk_search_ary_bot")
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "5079629749"))
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://aryan1082:aryan_1082@cluster0.jmqpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
